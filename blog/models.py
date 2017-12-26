2d4d15a364ee89e8c8d78116e1ee06d2a7e16b9e
Jun-seon <hjs0579@ajou.ac.kr>
2017-12-26 17:24:40 +0900

    html

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 
+#foreingkey 일 때, typeError 발생
+#on_delete 옵션 추가해야됨 (왜인지는 잘 모르겠음)
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
